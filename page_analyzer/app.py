597fd61f64c87c2f7208eacb01d695875068c3dc
Vladislav <balenkov53@gmail.com>
2024-11-16 22:36:50 +0300

    add template with bootstrap

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,4 +9,4 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def index():
-    return "Hello"
+    return render_template('index.html')
