bac1ad654bd26eb46fc3d0d1ae75518c49b9ce64
Vladislav <balenkov53@gmail.com>
2024-11-21 07:40:31 +0300

    add urls_check table

--- database.sql
+++ database.sql
@@ -1,5 +1,14 @@
-CREATE TABLE IF NOT EXISTS urls(
+CREATE TABLE IF NOT EXISTS urls (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(255) NOT NULL,
     created_at date NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS url_checks (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id bigint REFERENCES urls(id) NOT NULL,
+    status_code int NOT NULL,
+    h1 varchar(255),
+    title varchar(500),
+    description text
 );
